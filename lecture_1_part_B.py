f473f01a1c99adc5acb8150131fc9469f579d19d
moshekagan <shaikagan!1>
2021-10-17 13:43:28 +0300

    add to lecture 1

--- lecture_1_part_B.py
+++ lecture_1_part_B.py
@@ -37,13 +37,47 @@ def find_common_grade(students):
     return grade
 
 
+def find_avg(students):
+    sum = 0
+    for grade in students.values():
+        sum += int(grade)
+
+    return sum / len(students)
+
+
+def students_with_common_grade(students, common_grade):
+    common_students = []
+
+    for name, grade in students.items():
+        if common_grade == grade:
+            common_students.append(name)
+
+    return common_students
+
+
+def add_student_to_csv(name, grade):
+    with open('lecture_1_students_2_grads.csv', 'a+', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+
+        writer.writerow([name, grade])
+        csvfile.close()
+
+
 def main():
     print_all_student_from_csv()
     students = read_csv_to_dict()
-    print(students)
+    print() # empty line
 
-    print("The most common grade is: " + find_common_grade(students))
+    common_grade = find_common_grade(students)
+    print(f"The most common grade is: {common_grade}")
+    print(f"The Students with the common grades are: {students_with_common_grade(students, common_grade)}")
 
+    print()  # empty line
+    print(f"The average grades is: {find_avg(students)}")
+
+    print()  # empty line
+    add_student_to_csv("Jerry", 91)
+    print_all_student_from_csv()
 
 if __name__ == '__main__':
     main()
