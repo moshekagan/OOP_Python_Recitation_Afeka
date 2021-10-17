f03fe42b66e648b8fea459ba45693e74b68c1e29
moshekagan <shaikagan!1>
2021-10-17 13:44:22 +0300

    Lecture 2 - part 1

---
+++ Lecture_2/main.py
@@ -0,0 +1,10 @@
+from Lecture_2.Player import Player
+
+
+def main():
+   player = Player("Antony Parker", 111, 202, ["SG", "SF"], 1975)
+   player.print_me()
+
+
+if __name__ == '__main__':
+    main()
