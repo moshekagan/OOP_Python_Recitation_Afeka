0788d9a80bd084859da920052a77b0206f323f7c
moshekagan <shaikagan!1>
2021-12-19 14:47:15 +0200

    improve app

--- Lecture_6_ui/my_app.py
+++ Lecture_6_ui/my_app.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.ttk as ttk
 
 from Lecture_6_ui.League import League
 from Lecture_6_ui.Team import Team
@@ -11,21 +12,32 @@ def handel_create_team_click():
     text = f"You need to enter a team name!"
     fg = "red"
 
-    if team_name_input.get():
-        team = Team(team_name_input.get())
+    # Adding to combobox id checkbox is check
+    if enable_add_team_value.get():
+        if team_name_input.get():
+            team = Team(team_name_input.get())
 
-        res = league.add_team(team)
-        if res:
-            text = f"Created new team: {team.name}!"
-            fg = "green"
+            res = league.add_team(team)
+            if res:
+                text = f"Created new team: {team.name}!"
+                fg = "green"
 
-            new_team_lbl = Label(frame, text=team.name)
-            new_team_lbl.grid(row=len(league.teams)+4, column=0, sticky='nsew')
-            frame.rowconfigure(len(league.teams))
+                # Update label
+                new_team_lbl = Label(frame, text=team.name)
+                new_team_lbl.grid(row=len(league.teams)+4, column=0, sticky='nsew')
 
-            print(league.teams)
-        else:
-            text = f"Team {team.name} already exist"
+                # Add team name to combobox list
+                teams_combo.configure(values=[t.name for t in league.teams])
+
+                # Display all teams in a list (Bonus)
+                frame.rowconfigure(len(league.teams))
+
+                print(league.teams)
+            else:
+                text = f"Team {team.name} already exist"
+    else:
+        text = "You need to enable adding team..."
+        fg = "orange"
 
     status_label.configure(text=text, fg=fg)
 
@@ -47,9 +59,16 @@ team_name_input = Entry(win, width=15)
 team_name_input.grid(column=0, row=2)
 team_name_input.focus()
 
+teams_combo = ttk.Combobox(win, values=[])
+teams_combo.grid(row=5, column=0)
+
+enable_add_team_value = BooleanVar(value=True)
+enable_add_team_checkbox = ttk.Checkbutton(win, text="Enable add team", var=enable_add_team_value)
+enable_add_team_checkbox.grid(row=6, column=0)
+
 frame = Frame(win)
 frame.columnconfigure(1, weight=3)
-frame.grid(row=5, column=0, sticky='nsew', rowspan=2)
+frame.grid(row=7, column=0, sticky='nsew', rowspan=2)
 
 win.mainloop()
 print("App end")
